13158d795608f44c4399db6a21e25243a445dbf9
cmsjulio <cms.julio1@gmail.com>
2022-03-07 11:42:10 -0300

    Conceitos iniciais de Exceptions.

--- src/pck10exceptionsInJava/CheckedExceptionDemo.java
+++ src/pck10exceptionsInJava/CheckedExceptionDemo.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 
 public class CheckedExceptionDemo {
   public static void main(String[] args) throws MyException { //let the whoever caller is calling it deals with the exception
-    FileReader file = new FileReader("/home/wmancciny/file2");
-    BufferedReader fileInput = new BufferedReader(file);
-
-    // Print first 3 lines of file
-    for (int counter = 0 ; counter < 0; counter++)
-      System.out.println(fileInput.readLine());
-    fileInput.close();
+//    FileReader file = new FileReader("/home/wmancciny/file2");
+//    BufferedReader fileInput = new BufferedReader(file);
+//
+//    // Print first 3 lines of file
+//    for (int counter = 0 ; counter < 0; counter++)
+//      System.out.println(fileInput.readLine());
+//    fileInput.close();
 
     throw new MyException(); //to throw MyException
 
